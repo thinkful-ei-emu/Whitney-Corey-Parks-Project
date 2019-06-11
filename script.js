7972fa4754d3e88dfa93db380866e8b6fa4734ce
Whitney Sam <whitneysam@Whitneys-MacBook-Pro.local>
2019-06-11 16:20:01 -0700

    setup files/initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * 1. User must be able to search for parks in one or more states
+ * 2. User must be able to set the max num of results, with a default of 10
+ * 3. The search must trigger a call to NPS's API
+ * 4. The parks in the given state must be displayed on the page
+ * AND include at least: full name, description, website URL
+ * 5. User must be able to make multiple searches and see only the results for the current search
+ * 6. Stretch goal: add park's address to the results
+ */
